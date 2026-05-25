657e277a3ac613a0b87ce29b9f423f919667b94c
Harsh Manek <harsh.manek@simformsolutions.com>
2026-05-25 19:22:04 +0530

    added azuread authentication for connecting storage account to user without using access keys and storing the state in remote in storage account

--- main.tf
+++ main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~> 4.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "tfstate"
+    storage_account_name = "tfstate9101"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+    use_azuread_auth     = true
+  }
   required_version = ">= 1.1.0"
 }
 
